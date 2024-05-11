a6eec19924638338b036e292addb43ab55e2e3b1
Chen Yufei <cyfdecyf@gmail.com>
2024-05-11 12:58:51 +0800

    cli: add import command.

---
+++ src/fotil/cli/importer.py
@@ -0,0 +1,35 @@
+from typing import Annotated, Optional
+
+import typer
+
+from fotil import config
+from fotil.importer import Importer
+
+from . import app, state
+
+
+def _import(import_conf: config.ImportConfig, dry_run: bool):
+    imp = Importer(import_conf, verbose=state['verbose'], dry_run=dry_run)
+    imp.import_to_dst()
+
+
+@app.command(name='import')
+def importer(
+    imp: Annotated[
+        Optional[str], typer.Option('--importer', '-i', help='Name of the importer.')  # noqa: UP007
+    ] = None,
+    dry_run: Optional[bool] = False,  # noqa: UP007
+):
+    """
+    Import src_dir in import config to dst_dir.
+    """
+    config = state['config']
+    if imp and imp not in config.importer:
+        print(f'importer "{imp}" not found in config')
+        typer.Exit(code=1)
+
+    imps = config.importer.values() if imp is None else [config.importer[imp]]
+
+    for i in imps:
+        if i.enabled:
+            _import(i, dry_run)
