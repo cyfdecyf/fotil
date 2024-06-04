ebd2b9ea04de208c35516763138ce7a5d0792d15
Chen Yufei <cyfdecyf@gmail.com>
2024-06-04 21:56:10 +0800

    fix(cleanup-raw): correcting path resolve.

--- src/fotil/library.py
+++ src/fotil/library.py
@@ -24,19 +24,26 @@ class Library:
             raw_dir (Path): directory containing raw files to clean up
             pic_dir (Path): directory containing processed pictures
         """
+        print(f'cleaning up: {raw_dir}')
         pic_files = {
             f.stem
             for f in iter_directory_files(pic_dir, file_filter=self.conf.pic_file_filter)
         }
 
+        trash_dir_printed = False
+
         for f in iter_directory_files(raw_dir, file_filter=self.conf.raw_file_filter):
             if f.stem not in pic_files:
                 continue
 
-            src_dir = f.absolute().parent.relative_to(self.conf.raw_dir)
-            dst_dir = self.conf.trash_dir / src_dir
-            if self.verbose:
-                print(f'trash {f} to {dst_dir}')
+            src = (raw_dir / f).absolute()
+            src_reldir = src.relative_to(self.conf.raw_dir).parent
+            trash_dir = self.conf.trash_dir / src_reldir
+            if not trash_dir_printed:
+                print(f'trash_dir: {trash_dir}')
+                trash_dir_printed = True
+
+            print(f'trashing {f}')
             if not self.dry_run:
-                dst_dir.mkdir(parents=True, exist_ok=True)
-                f.rename(dst_dir / f.name)
+                trash_dir.mkdir(parents=True, exist_ok=True)
+                src.rename(trash_dir / src.name)
